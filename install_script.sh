7d6d844efb80dd100951d7158e1ab2df1eead185
Konstantin Lomov <79058995061@yandex.ru>
2021-09-02 23:04:49 +0500

    an approximate sketch of the script

---
+++ install_script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#install work soft
+
+apt-get install vim
+
+apt-get install emacs
+
+apt-get install tmux
+
+apt-get install git
+
+apt-get install links
+
+apt-get install telegram-cli
