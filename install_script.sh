7351ad380c46739bd9f4219f78459f4b2e7c6bb6
Konstantin Lomov <79058995061@yandex.ru>
2022-04-10 20:40:04 +0500

    added OS check

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#ghp_dAoYJ0BvrhpiJ8iXg7Pzg86w5DqD4B1PxdU://habr.com/ru/post/54649/ ! /bin/bash
 
 #install work soft
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # ...
 apt-get install vim
 
 apt-get install emacs
@@ -13,3 +14,6 @@ apt-get install git
 apt-get install links
 
 apt-get install telegram-cli
+
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
