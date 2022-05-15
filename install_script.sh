47e44eaaf8ee6cebc066e63c0e6c61036e083723
Konstantin Lomov <79058995061@yandex.ru>
2022-05-15 23:27:10 +0500

    continuation of work on the project

--- install_script.sh
+++ install_script.sh
@@ -4,12 +4,12 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then 
     RELISE=`cat /etc/*-release| tr "\n" " "`
 
-    for name in 'UBUNTU' 'DEBIAN'
+    for name in 'UBUNTU' 'DEBIAN' 'Fedora' 'ARCH'
     do
             name_os=`$RELISE | grep -i -q $name`
     done
     
-    if  [[ $name_os == "UBUNTU"  $name_os == "DEBIAN" ]]; then
+    if  [[ $name_os == "UBUNTU" || $name_os == "DEBIAN" ]]; then
         apt-get install vim
 
         apt-get install emacs
@@ -18,8 +18,42 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
         apt-get install git
 
-        apt-get install links
+        apt-get install mc
 
-        apt-get install telegram-cli
+        apt-get install fish
+    elif [[ $name_os == "Fedora" ]]; then
+        dnf install vim
+
+        dnf install emacs
+
+        dnf install tmux
+
+        dnf install git
+
+        dnf install mc
+
+        dnf install fish
+    elif [[ $name_os == "ARCH" ]]; then
+
+        pacman -S vim
+
+        pacman -S emacs
+
+        pacman -S tmux
+
+        pacman -S git
+
+        pacman -S mc
+
+        pacman -S fish
+    fi
 
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
+
+    echo "..."
+
+fi
+
+
+
+
